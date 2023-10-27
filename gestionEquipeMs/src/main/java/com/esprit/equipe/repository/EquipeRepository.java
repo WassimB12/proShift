d799dd74ad926081a1f7f7547da72331ac1f98c4
YassineMsadaa <yassine.msadaa@esprit.tn>
2023-10-27 12:25:04 +0100

    first commit

--- gestionEquipeMs/src/main/java/com/esprit/equipe/repository/EquipeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.esprit.equipe.repository;
-
-import com.esprit.equipe.model.Equipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EquipeRepository extends JpaRepository<Equipe, Long> {
-}
--- gestionTacheMs/src/main/java/com/esprit/gestionTacheMs/repository/TacheRepository.java
+++
@@ -1,8 +0,0 @@
-package com.esprit.gestionTacheMs.repository;
-
-
-import com.esprit.gestionTacheMs.model.Tache;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TacheRepository extends JpaRepository<Tache, Long> {
-}
